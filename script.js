773ca37fe9bebecdcf32d68517f2163ea447a8da
sykul <luke.flatley@outlook.com>
2024-03-16 17:35:54 +0100

    boilerplate and create gameboard

---
+++ script.js
@@ -0,0 +1,13 @@
+const gameBoard = (function() {
+    const topLeft = '';
+    const topMiddle = '';
+    const topRight = '';
+    const middleLeft = '';
+    const middleMiddle = '';
+    const middleRight = '';
+    const bottomLeft = '';
+    const bottomMiddle = '';
+    const bottomRight = '';
+
+    return { topLeft, topMiddle, topRight, middleLeft, middleMiddle, middleRight, bottomLeft, bottomMiddle, bottomRight };
+})();
