e3e94db0a74af43f45727b28dd3491ad9c6aeaa7
sykul <luke.flatley@outlook.com>
2024-03-21 19:04:08 +0100

    give createPlayer factory function an increaseScore method

--- script.js
+++ script.js
@@ -9,14 +9,15 @@ const gameBoard = (function() {
         }
     }
 
-
     return boardArray;
 })();
 
 function createPlayer(name, OorX) {
     const playerName = name;
     const symbol = OorX;
-    return {playerName, symbol}
+    let score = 0;
+    const increaseScore = () => ++score;
+    return {playerName, symbol, increaseScore}
 }
 
 const gameController = (function() {
@@ -27,12 +28,25 @@ const gameController = (function() {
     function trackPlayerScore() {
         
     }
+    const currentPlayer = 'player1'
 
+
+    return {currentPlayer}
 })();
 
-const screenController = (function() {
-
+const displayController = (function() {
+    
 })();
 
 const player1 = createPlayer('player1', 'X');
 const player2 = createPlayer('player2', 'O');
+
+for (let i = 0; i<50; i++) {
+    if (gameController.currentPlayer == 'player1') {
+        console.log(gameController.currentPlayer);
+        gameController.currentPlayer = 'player2'
+    } else if (gameController.currentPlayer == 'player2') {
+        console.log(gameController.currentPlayer);
+        gameController.currentPlayer = 'player1'
+    }
+}
