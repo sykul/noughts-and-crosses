8c1fcc12d22f753455d4d91688d0606090bc1b21
sykul <luke.flatley@outlook.com>
2024-03-24 15:03:06 +0100

    define winning conditions

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const gameBoard = (function() {
-    const numberOfRows = 3;
-    const numberOfColumns = 3;
+function createGameBoard(numberOfRows, numberOfColumns) {
     const boardArray = []
     for (let i = 0; i < numberOfRows; i++) {
         boardArray[i] = [];
@@ -10,28 +8,84 @@ const gameBoard = (function() {
     }
 
     return boardArray;
-})();
+}
+
+const gameBoard = createGameBoard(3, 3)
 
 function createPlayer(name, OorX) {
     const playerName = name;
     const symbol = OorX;
     let score = 0;
     const increaseScore = () => ++score;
-    return {playerName, symbol, increaseScore}
+    const resetScore = () => score = 0;
+    return {playerName, symbol, increaseScore, resetScore}
 }
 
-const gameController = (function() {
-    function playRound() {
+const winningConditions = (function() {
+    const numberOfRows = 3;
+    const numberOfColumns = 3;
 
+    const oneFourSeven = createGameBoard(3, 3)
+    for (i in oneFourSeven) {
+        for (j in oneFourSeven[i]) {
+            if (j == 0) {
+                oneFourSeven[i][j] = 'true';
+            }
+        }
+    }
+
+    const twoFiveEight = []
+
+
+    const threeSixNine = []
+
+
+    const oneFiveNine = []
+
+
+    const threeFiveSeven = []
+
+
+    const oneTwoThree = []
+
+
+    const fourFiveSix = []
+
+
+    const sevenEightNine  = []
+
+
+    return {oneFourSeven, twoFiveEight, threeSixNine, oneFiveNine, threeFiveSeven, oneTwoThree, fourFiveSix, sevenEightNine}
+})()
+
+const gameController = (function() {
+    function checkWinningCondition() {
+        for (i in gameBoard) {
+             gameBoard[i];
+            return 
+        }
+    }
+
+    function playRound() { 
+        for (let i = 0; i<50; i++) {
+            if (gameController.currentPlayer == 'player1') {
+                console.log(gameController.currentPlayer);
+                gameController.currentPlayer = 'player2'
+            } else if (gameController.currentPlayer == 'player2') {
+                console.log(gameController.currentPlayer);
+                gameController.currentPlayer = 'player1'
+            }
+        }
     }
 
     function trackPlayerScore() {
         
     }
+
     const currentPlayer = 'player1'
 
 
-    return {currentPlayer}
+    return {currentPlayer, checkWinningCondition, playRound}
 })();
 
 const displayController = (function() {
@@ -40,13 +94,3 @@ const displayController = (function() {
 
 const player1 = createPlayer('player1', 'X');
 const player2 = createPlayer('player2', 'O');
-
-for (let i = 0; i<50; i++) {
-    if (gameController.currentPlayer == 'player1') {
-        console.log(gameController.currentPlayer);
-        gameController.currentPlayer = 'player2'
-    } else if (gameController.currentPlayer == 'player2') {
-        console.log(gameController.currentPlayer);
-        gameController.currentPlayer = 'player1'
-    }
-}
